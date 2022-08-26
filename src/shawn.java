191a29fc79592f3fcd4432704204f2e529a0e0a5
sandhus <shummy.sandhu@hotmail.com>
2022-08-25 19:19:07 -0700

    shawn's rev

---
+++ src/shawn.java
@@ -0,0 +1,26 @@
+public class shawn {
+
+    public static void main(String[] args) {
+
+            int studentAge = 27;
+            double studentGPA = 3.6;
+            char studentfirstint = 's';
+            char studentlastint = 'u';
+            boolean perfectattend = false;
+            String name = "Shawn Sandhu";
+
+        System.out.println("student's name is "+ name);
+            System.out.println("student's age is "+ studentAge);
+            System.out.println("student's GPA is "+ studentGPA);
+            System.out.println("student's first initial is "+ studentfirstint);
+            System.out.println("student's last initial is "+ studentlastint);
+            System.out.println("student's attendence is "+ studentAge);
+            if (perfectattend) {
+                System.out.println("student's attendance is perfect");
+            }
+            else {
+                System.out.println("student's attendance is NOT perfect");
+            }
+
+    }
+}
